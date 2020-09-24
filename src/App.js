1969066db6673e278aecbbef39e1c9e5a19aca1f
Eder Gabriel <eder1602@outlook.com>
2020-09-23 23:44:00 -0300

    Funcionalidade de carta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  function handleLogout() {
+    var card = document.querySelector('.card');
+    card.classList.toggle('is-flipped');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div class="scene">
+    <div class="card" onClick={handleLogout}>
+      <div class="card__face card__face--front">front</div>
+      <div class="card__face card__face--back">back</div>
     </div>
+  </div>
   );
 }
 

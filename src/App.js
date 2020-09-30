6cc1a59e52dfa91971839fb512bee3943a2c3081
Eder Gabriel <eder1602@outlook.com>
2020-09-29 23:59:15 -0300

    Ajuste de carta

--- src/App.js
+++ src/App.js
@@ -8,19 +8,32 @@ function App() {
   let openCard = false;
   
   function handleClick(e) {
+    console.log(e);
     e.preventDefault();
     e.currentTarget.classList.toggle('is-flipped');
-    if(openCard) {
+
+
+
+    if(!openCard) {
       openCard = true;
     } else {
-      
+      var element = document.querySelectorAll('.card.is-flipped')
+      setTimeout(function() {
+        Array.prototype.forEach.call( element, function( node ) {
+          //node.parentNode.removeChild( node );
+          console.log(node.parentNode, element)
+          node.classList.remove('is-flipped')
+        });
+        openCard = false
+      }, 1000)
+
     }
 
   }
 
   return (
   <div>
-    <div className="card" onClick={handleClick} id='1'>
+    <div className="card"   onClick={handleClick} id='1'>
       <div className="card__face card__face--front">front</div>
       <div className="card__face card__face--back">back</div>
     </div>

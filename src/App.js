ddcc9df5b09f1ef47c43e9bba619d9abe9c24f05
Eder Gabriel <eder1602@outlook.com>
2020-10-10 00:00:01 -0300

    Misturar o Array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
 
+  return array
+}
 
 function App() {
 
   let openCard = false;
   let validClick = 0;
+
+  //(Math.random() * 5.99).toFixed(0)
+  //a.splice(1,0, 222)
   
+  let positions = [
+    {id: 1, title: "e"},
+    {id: 1, title: "e"},
+    {id: 2, title: "s"},
+    {id: 2, title: "s"},
+    {id: 3, title: "a"},
+    {id: 3, title: "a"}];
+
+  positions = shuffleArray(positions);
+
+  console.log(positions);
+
   function handleClick(e) {
     e.preventDefault();
     validClick = validClick + 1;
